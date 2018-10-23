a525aca2a41ffeb889b7ddf3a57284f998a1b191
Uchenna Okafor <okafor.u@hotmail.com>
2018-10-23 14:12:22 +0100

    * Added controllers for both the student and teacher routes

---
+++ MitigatingCircumstances/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MitigatingCircumstances.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
